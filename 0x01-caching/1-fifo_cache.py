4cddc501b13f56d7027b9bb0db8c27769607fd2a
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-17 16:43:04 +0300

    caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""  class FIFOCache that inherits from BaseCaching
+"""
+
+from collections import deque
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ class FIFOCache  
+    """
+
+    def __init__(self):
+        """ Init
+        """
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        """ Must assign to the dictionary for
+        the key key.
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif self.is_full():
+                self.evict()
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Must return the value  
+        """
+        return self.cache_data.get(key, None)
+
+    def is_full(self):
+        """ If the number of items in self.cache_data is higher that
+        BaseCaching.MAX_ITEMS
+        """
+        return len(self.cache_data) >= self.MAX_ITEMS
+
+    def evict(self):
+        """ you must print DISCARD:  
+        new line
+        """
+        popped = self.queue.popleft()
+        del self.cache_data[popped]
+        print("DISCARD: " + str(popped))
+        

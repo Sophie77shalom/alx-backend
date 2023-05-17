7e4093ddbe318a54a5cd7269f3d90b97deff8247
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-17 16:52:32 +0300

    cacching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" class LRUCache that inherits from BaseCaching
+"""
+
+from collections import deque
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Create a class LRUCache  
+     
+    """
+
+    def __init__(self):
+        """ Init
+        """
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        """ Must assign to the dictionary  
+         
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif self.is_full():
+                self.evict()
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Must return the value  
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data.get(key)
+
+    def is_full(self):
+        """ If the number of items in self.cache_data is higher that
+        BaseCaching.MAX_ITEMS
+        """
+        return len(self.cache_data) >= self.MAX_ITEMS
+
+    def evict(self):
+        """ you must print DISCARD: 
+        new line
+        """
+        popped = self.queue.popleft()
+        del self.cache_data[popped]
+        print("DISCARD: " + str(popped))
